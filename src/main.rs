382731994776af51246766aa98ef59a8e7a1617d
Matt Wright <mattwright3195@gmail.com>
2020-09-27 16:38:37 -0700

    run rust fmt

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 // docker run -p 8080:80 gcrtest
 // docker stop $(docker ps -a -q)
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use actix_cors::Cors;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -14,11 +14,7 @@ async fn hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     println!("Starting server...");
 
-    HttpServer::new(|| {
-        App::new()
-            .wrap(Cors::new().finish())
-            .service(hello)
-    })
+    HttpServer::new(|| App::new().wrap(Cors::new().finish()).service(hello))
         .bind("0.0.0.0:8080")?
         .run()
         .await
